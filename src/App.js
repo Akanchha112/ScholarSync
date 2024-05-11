02a37cb3305f2c240175219cf6d856e4a74f2ce6
Tarni Balgoher <92452739+tarnibalgoher5@users.noreply.github.com>
2024-05-11 12:00:27 +0000

    job search page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import LandingPage from './pages/LandingPage/LandingPage';
-import SigninPage from './pages/SigninPage/SigninPage';
-import SignupPage from './pages/SignupPage/SignupPage';
+// import LandingPage from './pages/LandingPage/LandingPage';
+// import SigninPage from './pages/SigninPage/SigninPage';
+// import SignupPage from './pages/SignupPage/SignupPage';
 import {ToastContainer} from "react-toastify";
+import JobSearch from './pages/JobSearch/JobSearch';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />}/>
+        {/* <Route path="/" element={<LandingPage />}/>
         <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/signin" element={<SigninPage/>} />
+        <Route path="/signin" element={<SigninPage/>} /> */}
+        <Route path="/" element={<JobSearch/>}/>
       </Routes>
       <ToastContainer />
     </BrowserRouter>
---
+++ src/pages/JobSearch/JobSearch.jsx
@@ -0,0 +1,68 @@
+import './JobSearch.css';
+import Navbar from '../../components/Navbar/Navbar';
+import React, { useState } from 'react';
+// import search from '../../components/img/search.jpg';
+import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+const JobSearch = () => {
+    const navigate=useNavigate();
+    const ViewDetails=()=>{
+        navigate('/');
+    }
+    const [searchQuery, setSearchQuery] = useState('');
+  
+    const [cards, setCards] = useState([
+      { id: 1, Jobtitle: 'Jobtitle 1', salary: '$123' , Jobstatus:'Open' },
+      {id: 2, Jobtitle: 'Jobtitle 2', salary: '$123' , Jobstatus:'Open'}
+    ]);
+  
+    const handleSearchInputChange = (e) => {
+      setSearchQuery(e.target.value);
+    };
+ 
+    const filteredCards = cards.filter((card) =>
+        card.Jobtitle.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    
+    const handleCardClick = (id) => {
+        history.push(`/details/${id}`); // You can specify the route you want to navigate to
+      };
+    
+    return <>
+      <Navbar/>
+      <div className="JobSearch">
+        <div className='search'>
+        {/* <img src={search} className='searchIcon' alt=""/> */}
+        <input
+          type="text"
+          placeholder="Search for new opportunities "
+          id='searchBar'
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        
+        />
+        
+        </div>
+        
+        <div className="card-container">
+          {filteredCards.map((card) => (
+            <div key={card.id} className="card">
+              <h2 className='Jobtitle'>{card.Jobtitle}</h2>
+
+              <p  className='salary'>{card.salary}</p>
+              
+              <div className='JobstatusDiv'>
+                <label className=''>Status:</label>
+                <p  className='Jobstatus'>{card.Jobstatus}</p>
+                <button onClick={()=>{ViewDetails()}}>View Details</button>
+              </div>
+            
+              
+            </div>
+          ))}
+        </div>
+      </div>
+  </>
+  };
+  
+  export default JobSearch;
