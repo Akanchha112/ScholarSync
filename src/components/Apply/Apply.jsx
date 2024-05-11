8c491d8052e19d21f539bbdc859f831f7cdae58b
Tarni Balgoher <92452739+tarnibalgoher5@users.noreply.github.com>
2024-05-11 22:58:00 +0000

    application form css done

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import './Apply.css';
+import Navbar from "../../components/Navbar/Navbar";
 const ApplyForm = ({ onSubmit }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit();
   };
 
-  return (
+  return <>
+  <Navbar />
     <div className="apply-form-container">
       <div className="apply-form-card">
         <h2>Application</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="question">Why should you be hired for this role?</label>
+          <label  className='formQuestion' htmlFor="question">Why should you be hired for this role?</label>
           <textarea id="question" rows="4" required></textarea>
           <br />
-          <label htmlFor="availability">Are you available for the stipulated time period?</label>
+          <label  className='formQuestion' htmlFor="availability">Are you available for the stipulated time period?</label>
           <textarea id="availability" rows="4" required></textarea>
           <button type="submit">Submit</button>
         </form>
       </div>
     </div>
-  );
+    </>
 };
 
 export default ApplyForm;
